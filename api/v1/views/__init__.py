6adc3c7579f3af482f86529b51da26e548be0078
oladakinjoye <oladakinjoye@gmail.com>
2024-05-01 17:08:05 -0700

    update api 33

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-'''The blueprint for the AirBnB clone API.
-'''
+'''Contains the blueprint for the API.'''
+from flask import Blueprint
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+'''The blueprint for the AirBnB clone API.'''
+
+
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
@@ -9,9 +15,3 @@ from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
-
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-'''Contains the blueprint for the API.
-'''
